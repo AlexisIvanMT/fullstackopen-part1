395d9dd13cf5c833de747210df054d0394f49c9b
AlexisIvanMT <84108058+AlexisIvanMT@users.noreply.github.com>
2025-11-12 22:24:43 -0600

    Add App component to 1.1-1.5

---
+++ 1.1-1.5/App.jsx
@@ -0,0 +1,67 @@
+// Componente para el encabezado
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+// Componente para una sola parte
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  )
+}
+
+// Componente para todas las partes
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
+    </div>
+  )
+}
+
+// Componente para el total
+const Total = (props) => {
+  const total = props.parts.reduce((sum, part) => sum + part.exercises, 0)
+  return <p>Number of exercises {total}</p>
+}
+
+const App = () => {
+  
+  const course = {
+    name:'Half Stack application development',
+    parts:[
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+      
+    ]
+
+  }
+    
+    
+  
+
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
